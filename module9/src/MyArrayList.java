f5e35df4af9ba714a59c0f1c9aa011a948accb3f
Dmytro <dimaborodavkin25@gmail.com>
2023-02-27 15:19:21 +0200

    8, 9, 10

--- module9/src/MyArrayList.java
+++ module9/src/MyArrayList.java
@@ -1,45 +1,60 @@
 public class MyArrayList<T> {
     int size = 0;
+    int pointer = 0;
     public Object[] arr;
 
     public MyArrayList() {
         arr = new Object[10];
     }
+
     public MyArrayList(int initialSize) {
         if (initialSize > 0) {
             arr = new Object[initialSize];
         } else if (initialSize == 0) {
             arr = new Object[]{};
         } else {
-            throw new IllegalArgumentException("Illegal Capacity: "+
+            throw new IllegalArgumentException("Illegal Capacity: " +
                     initialSize);
         }
     }
 
     public void add(T value) {
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == null) {
-                arr[i] = value;
-                size++;
-                break;
+        if (pointer == arr.length - 1) {
+            Object[] newArr = new Object[arr.length * 2];
+            System.arraycopy(arr, 0, newArr, 0, pointer);
+            arr = newArr;
+        }
+        arr[pointer++] = value;
+        size++;
+    }
+
+    public void remove(int index) throws ArrayIndexOutOfBoundsException {
+        if (index >= 0 && index <= size) {
+            for (int i = index; i < size; i++) {
+                arr[i] = arr[i + 1];
             }
+            size--;
+        } else {
+            throw new ArrayIndexOutOfBoundsException();
         }
     }
-    public void remove(int index) {
-        for (int i = index; i < size; i++) {
-            arr[i] = arr[i + 1];
-        }
-        size--;
-    }
+
     public void clear() {
         for (int i = 0; i < size; i++) {
             arr[i] = null;
         }
+        size = 0;
     }
+
     public int size() {
         return size;
     }
-    public T get(int index) {
-        return (T) arr[index];
+
+    public T get(int index) throws ArrayIndexOutOfBoundsException {
+        if (index >= 0 && index <= size) {
+            return (T) arr[index];
+        } else {
+            throw new ArrayIndexOutOfBoundsException();
+        }
     }
 }
