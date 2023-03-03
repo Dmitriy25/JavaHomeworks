fd23694eadeb802817ded5c6ac010fd73b8ddac8
Dmytro <dimaborodavkin25@gmail.com>
2023-03-03 16:56:12 +0200

    module10

--- module10/Main/src/FileToJson.java
+++ module10/Main/src/FileToJson.java
@@ -1,2 +1,37 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class FileToJson {
+    private List<User> users = new ArrayList<>();
+
+    public void usersFromTxtToJson(String filePath) {
+
+        File file = new File(filePath);
+        InputStream fis = null;
+        try {
+            fis = new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        Scanner scanner = new Scanner(fis);
+        scanner.nextLine();
+        while (scanner.hasNext()) {
+            String[] userNameAgeArr = scanner.nextLine().split(" ");
+            users.add(new User(userNameAgeArr[0], Integer.parseInt(userNameAgeArr[1])));
+        }
+        Writer fileWriter = null;
+        try {
+            fileWriter = new FileWriter("user.json");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        gson.toJson(users, fileWriter);
+        System.out.println(gson.toJson(users));
+    }
 }
--- module10/Main/src/Main.java
+++ module10/Main/src/Main.java
@@ -1,9 +1,24 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Task №1");
+        System.out.println("=-=-=-=-=");
+
         Validator v = new Validator();
         v.printValidNumbers("numbers.txt");
 
+        System.out.println("=-=-=-=-=");
+        System.out.println("\nTask №2");
+        System.out.println("=-=-=-=-=");
+
         Words w = new Words();
         w.wordsFrequency("words.txt");
+
+        System.out.println("=-=-=-=-=");
+        System.out.println("\nTask №3");
+        System.out.println("=-=-=-=-=");
+
+        FileToJson f = new FileToJson();
+        f.usersFromTxtToJson("file.txt");
+        System.out.println("=-=-=-=-=");
     }
 }
